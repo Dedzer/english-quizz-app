e0e747a483d680acfbd4116d3ccb9892cb3a3756
Vadym <shchur_vadym@ukr.net>
2019-08-19 12:10:01 +0300

    initial commit

---
+++ src/main/java/com/dedzer/englishQuizz/EnglishQuizzApplication.java
@@ -0,0 +1,13 @@
+package com.dedzer.englishQuizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishQuizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishQuizzApplication.class, args);
+	}
+
+}
