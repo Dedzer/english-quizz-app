3d00f13941f937dd118a9751731e425aaea98a26
Vadym <shchur_vadym@ukr.net>
2019-09-07 21:15:06 +0300

    added type and level to my tests

--- src/main/java/com/dedzer/englishQuizz/entity/Test.java
+++ src/main/java/com/dedzer/englishQuizz/entity/Test.java
@@ -10,6 +10,8 @@ public class Test {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private String level;
+    private String type;
     @Column(name = "video_url")
     private String videoUrl;
     @OneToMany(mappedBy = "test", cascade = CascadeType.PERSIST)
@@ -32,6 +34,22 @@ public class Test {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 
     public String getVideoUrl() {
